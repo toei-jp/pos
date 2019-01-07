33c0b6c271ebeff218cabb0cd38b212cdc19d1f5
Akito Hataguchi <hataguchi@motionpicture.jp>
2019-01-07 19:22:29 +0900

    TOEI-121 エラーハンドリングテスト

--- src/server/app/routes/authorize.ts
+++ src/server/app/routes/authorize.ts
@@ -6,7 +6,7 @@ import * as express from 'express';
 import * as authorize from '../controllers/authorize/authorize.controller';
 const router = express.Router();
 
-// router.post('/getCredentials', authorize.getCredentials);
+router.post('/getCredentials', authorize.getCredentials);
 router.get('/signIn', authorize.signIn);
 router.get('/signOut', authorize.signOut);
 
